b50a5f606ebb36cb664c1d9e2160e4f6abc08ac6
Anyelo Hernandez <anyelo.g.hernandez@gmail.com>
2022-02-10 00:25:02 -0600

    Se mejoro el tablero y se añadio teletranportacion

--- juego-pacman/src/juego/pacman/JuegoPacman.java
+++ juego-pacman/src/juego/pacman/JuegoPacman.java
@@ -14,6 +14,7 @@ public class JuegoPacman {
 //      Estructura base para el movimiento de Pacman.   
         int filas, columnas;
         String movimiento;
+        boolean exit=false;
         
         Random rand = new Random();
         
@@ -45,6 +46,14 @@ public class JuegoPacman {
 //          Se ingresa el nombre.
             System.out.println("Ingrese su nombre:  ");
             nombre[0] = Scn.next();
+            for(int i = 0; i <= 50; i++)
+                {
+                    if(nombre[i] != null){
+                        break;
+                    }else{
+                        nombre[i] = Scn.next();
+                    }
+                }
             
 //          Se ingresa la edad.
             System.out.println("Ingrese su edad: ");
@@ -72,129 +81,188 @@ public class JuegoPacman {
             }
 //          Creacion del tablero
             String[][] tablero = new String[filas][columnas];
-            String[][] pacman = new String[filas][columnas];
-//          Decide la posicion inicial de pacman            
-            PosX = rand.nextInt(filas);
+            String[][] pacman = new String[filas][columnas];         
+            while(true){
+            if(exit == true)
+            {
+                exit = false;
+                break;
+            }
+//          Decide la posicion inicial de pacman               
+            PosX = rand.nextInt(filas);//filas = 10 | 0, 1, 2,...,9
             PosY = rand.nextInt(columnas);
-            pacman[PosX][PosY] = " V";
+            pacman[PosX][PosY] = "V";
 //          Genera paredes dependiendo de cuantas filas y columnas hayan           
             RandX = rand.nextInt(columnas/2);
             RandY = rand.nextInt(filas/2);
             
-            while(true){
-                
-//          Parte de arriba
-            for(int fila = 0; fila < filas; fila++)
-            {
-                tablero[fila][0] = "**"; 
-            }
-//          Columnas y espacios en blanco
-            for(int columna = 1; columna < columnas; columna++)
-            {
-//              Espacio para el portal en las columnas (columna izquierda)                
-                if(columna != (columnas / 2)){
-                    tablero[0][columna] = "* "; 
-                }else{
-                    tablero[0][columna] = "  "; 
+            for(int ParedY = 0; ParedY <= RandY; ParedY++){
+                for(int ParedX = 0; ParedX <= RandX; ParedX++){
+                    
                 }
-//              Espacios en blanco
-                for (int fila = 1; fila < filas; fila++)
+            }
+            
+            while(true)
+            {
+                 if(exit == true){
+                        break;
+                    }
+//              Parte de arriba    
+                for(int fila = 0; fila < filas; fila++)
                 {
-                    if(PosX == fila && PosY == columna)
+                    tablero[fila][0] = "*"; 
+                }
+//              Columnas y espacios en blanco    
+                for(int columna = 1; columna < columnas; columna++)
+                {
+//                  Espacio para el portal en las columnas (columna izquierda)                    
+                    if(columna != (columnas / 2))
                     {
-                        tablero[fila][columna] = " V";
-                    }else{
-                        tablero[fila][columna] = "  ";
-                        if(fila == filas - 1)
+//                      Hace simetricas las columnas
+                        if(columnas % 2 == 0)
                         {
-//                          Espacio para el portal en las columnas (columna derecha)
-                            if(columna != (columnas / 2))
+                            tablero[0][columnas/2 - 1] = " ";
+                        }else{
+                            tablero[0][columnas/2 - 1] = " ";
+                            tablero[0][columnas/2 + 1] = " ";
+                        }
+                        tablero[0][columna] = "*"; 
+                    }else{
+                        tablero[0][columna] = " "; 
+                    }
+                    
+//                  Espacios en blanco    
+                    for (int fila = 1; fila < filas; fila++)
+                    {
+                        if(PosX == fila && PosY == columna)
+                        {
+                            tablero[fila][columna] = "V";
+                        }else{
+                            tablero[fila][columna] = " ";
+                            if(fila == filas - 1)
                             {
-                                tablero[tablero.length - 1][columna] = " *"; 
-                            }else{
-                                tablero[tablero.length - 1][columna] = "  "; 
+//                              Espacio para el portal en las columnas (columna derecha)    
+                                if(columna != (columnas / 2))
+                                {
+//                                  Hace simetrica las columnas                                    
+                                    if(columnas % 2 == 0){
+                                        tablero[filas - 1][columnas/2 - 1] = " ";
+                                    }else{
+                                        tablero[filas - 1][columnas/2 - 1] = " ";
+                                        tablero[filas - 1][columnas/2 + 1] = " ";
+                                    }
+                                    tablero[filas - 1][columna] = "*";
+                                }else{
+                                    tablero[filas - 1][columna] = " ";
+                                }
                             }
                         }
                     }
                 }
-            }
-//          Parte de abajo
-            for(int fila = 0; fila < filas; fila++)
-            {
-                tablero[fila][tablero[0].length - 1] = "**"; 
-            }
-            
-            
-//          Impresión de la tabla            
-            for(int columna = 0; columna < columnas; columna++){
-                for(int fila = 0; fila < filas; fila++){
-                    System.out.print(tablero[fila][columna]);
-                }
-                System.out.println(" ");
-            }
-//          Ingresa el movimiento
-            System.out.println("Ingrese el movimiento [WASD]: ");
-            movimiento = Scn.next();
-//          Cierra la partida actual
-            if("m".equals(movimiento) || "M".equals(movimiento))
-            {
-                break;
-            }
-//          Mueve el pacman
-            while(true)
-            {
-                
-                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                if("w".equals(movimiento) || "W".equals(movimiento))
+//              Parte de abajo    
+                for(int fila = 0; fila < filas; fila++)
                 {
-                    if(tablero[PosX][PosY - 1] != "**")
-                    {
-                        PosY = PosY - 1;
-                        break;
-                    }else{
-                        PosY = PosY;
-                        break;
+                    tablero[fila][tablero[0].length - 1] = "*"; 
+                }
+
+
+//              Impresión de la tabla                
+                for(int columna = 0; columna < columnas; columna++){
+                    for(int fila = 0; fila < filas; fila++){
+                        System.out.print(tablero[fila][columna] + " ");
                     }
+                    System.out.println("");
+                }
+//              Revisa si el pacman aparece dentro de una celda con valor *
+                if(tablero[PosX][PosY] == "*"){
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                    break;
+                }
+
+//              Mueve el pacman    
+                while(true)
+                {
                     
-                }
-                if("s".equals(movimiento) || "S".equals(movimiento))
-                {
-                    if(tablero[PosX][PosY + 1] != "**")
+//                  Ingresa el movimiento    
+                    System.out.println("Ingrese el movimiento [WASD][M para salir]: ");
+                    movimiento = Scn.next();
+  
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                    if(exit == true){
+                        
+                        break;
+                    }
+//                  Mueve hacia arriba                    
+                    if("w".equals(movimiento) || "W".equals(movimiento))
                     {
-                        PosY = PosY + 1;
+                        if(tablero[PosX][PosY - 1] != "*")
+                        {
+                            PosY = PosY - 1;
+                            break;
+                        }else{
+                            PosY = PosY;
+                            break;
+                        }
+
+                    }
+//                  Mueve hacia abajo
+                    else if("s".equals(movimiento) || "S".equals(movimiento))
+                    {
+                        if(tablero[PosX][PosY + 1] != "*")
+                        {
+                            PosY = PosY + 1;
+                            break;
+                        }else{
+                            PosY = PosY;
+                            break;
+                        }
+                    }
+//                  Mueve a la izquierda
+                    else if("a".equals(movimiento) || "A".equals(movimiento))
+                    {
+                        if(tablero[PosX - 1][PosY] != "*")
+                        {
+                            PosX = PosX - 1;
+                            if(PosX == 0)
+                            {
+                                PosX = filas - 2;
+                            }
+                            break;
+                        }else{
+                            PosX = PosX;
+                            break;
+                        }
+                    }
+//                  Mueve a la derecha
+                    else if("d".equals(movimiento) || "D".equals(movimiento))
+                    {
+                        if(tablero[PosX + 1][PosY] != "*")
+                        {
+                            PosX = PosX + 1;
+                            if(PosX == filas - 1)
+                            {
+                                PosX = 1;
+                            }
+                            break;
+                        }else{
+                            PosX = PosX;
+                            break;
+                        }
+                    
+                    }
+//                  Cierra la partida actual    
+                    else if("m".equals(movimiento) || "M".equals(movimiento))
+                    {
+                        exit = true;
                         break;
                     }else{
-                        PosY = PosY;
+                        System.out.println("Ingrese una tecla valida!");
                         break;
                     }
                 }
-                if("a".equals(movimiento) || "A".equals(movimiento))
-                {
-                    if(tablero[PosX - 1][PosY] != "* ")
-                    {
-                        PosX = PosX - 1;
-                        break;
-                    }else{
-                        PosX = PosX;
-                        break;
-                    }
-                }
-                if("d".equals(movimiento) || "D".equals(movimiento))
-                {
-                    if(tablero[PosX + 1][PosY] != " *")
-                    {
-                        PosX = PosX + 1;
-                        break;
-                    }else{
-                        PosX = PosX;
-                        break;
-                    }
-                }
-                
-            }
             }
         }
-    
+        }
         else if(menu_opcion == 2)
         {
 //          Impresion del historial
